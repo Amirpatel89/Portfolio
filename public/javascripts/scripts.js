ed4716c5f05c61bf35d0fef3e081cfefd0ad9b79
Amir Patel <amirpatel89@gmail.com>
2017-12-11 11:27:17 -0500

    so close..

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -2,7 +2,6 @@ $( document ).ready(function() {
 	$(".project1").hide()
 	$(".project2").hide()
 	$(".project3").hide()
-	$(".project-python").hide()
 
 
 });
@@ -83,94 +82,112 @@ const starFoodieSkills = {
 	caption: 'This is an app for easily finding the nearest high rated restaurant near your location.',
 	all: 'yes'
 }
-var projects = [finalProjectSkills, shoeboxSkills, starFoodieSkills]
+var projects = [pygameSkills, shoeboxSkills, starFoodieSkills]
 console.log(projects)
-function checkProjects(x){
-	returnArray = [];
-	returnCaptions = [];
-	projectString = "";
-	projectString2 = "";
-	projectString3 = "";
-	projectString4 = "";
-	for (i = 0; i < projects.length; i++){
-		if(projects[i][x] == 'yes'){
-			returnArray.push(projects[i].name)
-			returnCaptions.push(projects[i].caption)
-		}
+function organize(x){
+	if($(".project1").show()){
+		console.log('showing')
+		$('.project1').addClass('animated slideOutRight');
+		setTimeout(checkProjects, 1000, x);
 	}
-	console.log(returnCaptions)
-	projectString += returnArray[0];
-	var projectLink = "/" + projectString
-	projectString2 += returnArray[1];
-	var projectLink2 = "/" + projectString2
-	projectString3 += returnArray[2];
-	var projectLink3 = "/" + projectString3
-	projectString4 += returnArray[3];
-	var projectLink4 = "/" + projectString4
-	var projectObject = {
-		[projectString]: projectLink,
-		[projectString2]: projectLink2,
-		[projectString3]: projectLink3,
-		[projectString4]: projectLink4
-	};
-	var cleanProjectObject = _.omit(projectObject, [undefined]);
-	var cleanProjectCaptions = _.omit(returnCaptions, [undefined]);
-	console.log(cleanProjectObject)
-			if (Object.keys(cleanProjectObject)[0] && Object.keys(cleanProjectObject)[1] && Object.keys(cleanProjectObject)[2] !== undefined){
-				document.getElementById("project1Title").innerHTML = Object.keys(cleanProjectObject)[0];
-				document.getElementById("project1Pic").src = 'images/' + Object.keys(cleanProjectObject)[0] + '.jpg';
-				document.getElementById("project1Caption").innerHTML = cleanProjectCaptions[0];
-				document.getElementById("project2Title").innerHTML = Object.keys(cleanProjectObject)[1];
-				document.getElementById("project2Pic").src = 'images/' + Object.keys(cleanProjectObject)[1] + '.jpg';
-				document.getElementById("project2Caption").innerHTML = cleanProjectCaptions[1];
-				document.getElementById("project3Title").innerHTML = Object.keys(cleanProjectObject)[2];
-				document.getElementById("project3Pic").src = 'images/' + Object.keys(cleanProjectObject)[2] + '.jpg';
-				document.getElementById("project3Caption").innerHTML = cleanProjectCaptions[2];
-				$(".project1").removeClass("animated slideOutRight");
-				$(".project2").removeClass("animated slideOutRight");
-				$('.project1').addClass('animated slideInRight');
-				$('.project2').addClass('animated slideInRight');
-				$(".project1").show()
-				$(".project2").show()
-				$(".project3").removeClass("animated slideOutRight");
-				$('.project3').addClass('animated slideInRight');
-				$(".project3").show()
-				console.log('4')
-		}
-		if (Object.keys(cleanProjectObject)[0] && Object.keys(cleanProjectObject)[1] !== undefined && Object.keys(cleanProjectObject)[2] == undefined){
-			document.getElementById("project1Title").innerHTML = Object.keys(cleanProjectObject)[0];
-			document.getElementById("project1Pic").src = 'images/' + Object.keys(cleanProjectObject)[0] + '.jpg';
-			document.getElementById("project1Caption").innerHTML = cleanProjectCaptions[0];
-			document.getElementById("project2Title").innerHTML = Object.keys(cleanProjectObject)[1];
-			document.getElementById("project2Pic").src = 'images/' + Object.keys(cleanProjectObject)[1] + '.jpg';
-			document.getElementById("project2Caption").innerHTML = cleanProjectCaptions[1];
-			$(".project1").removeClass("animated slideOutRight");
-			$(".project2").removeClass("animated slideOutRight");
-			$('.project1').addClass('animated slideInRight');
-			$('.project2').addClass('animated slideInRight');
-			$(".project1").show()
-			$(".project2").show()
-			$('.project3').addClass('animated slideOutRight');
-			console.log('1')
-		}
-		else if (Object.keys(cleanProjectObject)[0] !== undefined && Object.keys(cleanProjectObject)[1] == undefined){
-			document.getElementById("project1Title").innerHTML = Object.keys(cleanProjectObject)[0];
-			document.getElementById("project1Pic").src = 'images/' + Object.keys(cleanProjectObject)[0] + '.jpg';
-			document.getElementById("project1Caption").innerHTML = cleanProjectCaptions[0];
-				$(".project1").removeClass("animated slideOutRight");
-				$(".project1").show()	
-				$('.project1').addClass('animated slideInRight');
+	// if (Object.keys(cleanProjectObject)[0] && Object.keys(cleanProjectObject)[1] && Object.keys(cleanProjectObject)[2] !== undefined){
+
+	// }
+	// if (Object.keys(cleanProjectObject)[0] && Object.keys(cleanProjectObject)[1] !== undefined && Object.keys(cleanProjectObject)[2] == undefined){
+
+	// }
+	// if (Object.keys(cleanProjectObject)[0] !== undefined && Object.keys(cleanProjectObject)[1] == undefined){
+
+	// }
+	// setTimeout(checkProjects(x)), 3000;
+	function checkProjects(x){
+			returnArray = [];
+			returnCaptions = [];
+			projectString = "";
+			projectString2 = "";
+			projectString3 = "";
+			projectString4 = "";
+			for (i = 0; i < projects.length; i++){
+				if(projects[i][x] == 'yes'){
+					returnArray.push(projects[i].name)
+					returnCaptions.push(projects[i].caption)
+				}
+			}
+			console.log(returnCaptions)
+			projectString += returnArray[0];
+			var projectLink = "/" + projectString
+			projectString2 += returnArray[1];
+			var projectLink2 = "/" + projectString2
+			projectString3 += returnArray[2];
+			var projectLink3 = "/" + projectString3
+			projectString4 += returnArray[3];
+			var projectLink4 = "/" + projectString4
+			var projectObject = {
+				[projectString]: projectLink,
+				[projectString2]: projectLink2,
+				[projectString3]: projectLink3,
+				[projectString4]: projectLink4
+			};
+			var cleanProjectObject = _.omit(projectObject, [undefined]);
+			var cleanProjectCaptions = _.omit(returnCaptions, [undefined]);
+			console.log(cleanProjectObject)
+					if (Object.keys(cleanProjectObject)[0] && Object.keys(cleanProjectObject)[1] && Object.keys(cleanProjectObject)[2] !== undefined){
+						document.getElementById("project1Title").innerHTML = Object.keys(cleanProjectObject)[0];
+						document.getElementById("project1Pic").src = 'images/' + Object.keys(cleanProjectObject)[0] + '.jpg';
+						document.getElementById("project1Caption").innerHTML = cleanProjectCaptions[0];
+						document.getElementById("project2Title").innerHTML = Object.keys(cleanProjectObject)[1];
+						document.getElementById("project2Pic").src = 'images/' + Object.keys(cleanProjectObject)[1] + '.jpg';
+						document.getElementById("project2Caption").innerHTML = cleanProjectCaptions[1];
+						document.getElementById("project3Title").innerHTML = Object.keys(cleanProjectObject)[2];
+						document.getElementById("project3Pic").src = 'images/' + Object.keys(cleanProjectObject)[2] + '.jpg';
+						document.getElementById("project3Caption").innerHTML = cleanProjectCaptions[2];
+						$(".project1").removeClass("animated slideOutRight");
+						$(".project2").removeClass("animated slideOutRight");
+						$('.project1').addClass('animated slideInRight');
+						$('.project2').addClass('animated slideInRight');
+						$(".project1").show()
+						$(".project2").show()
+						$(".project3").removeClass("animated slideOutRight");
+						$('.project3').addClass('animated slideInRight');
+						$(".project3").show()
+						console.log('4')
+				}
+				else if (Object.keys(cleanProjectObject)[0] && Object.keys(cleanProjectObject)[1] !== undefined && Object.keys(cleanProjectObject)[2] == undefined){
+					document.getElementById("project1Title").innerHTML = Object.keys(cleanProjectObject)[0];
+					document.getElementById("project1Pic").src = 'images/' + Object.keys(cleanProjectObject)[0] + '.jpg';
+					document.getElementById("project1Caption").innerHTML = cleanProjectCaptions[0];
+					document.getElementById("project2Title").innerHTML = Object.keys(cleanProjectObject)[1];
+					document.getElementById("project2Pic").src = 'images/' + Object.keys(cleanProjectObject)[1] + '.jpg';
+					document.getElementById("project2Caption").innerHTML = cleanProjectCaptions[1];
+					$(".project1").removeClass("animated slideOutRight");
+					$(".project2").removeClass("animated slideOutRight");
+					$('.project1').addClass('animated slideInRight');
+					$('.project2').addClass('animated slideInRight');
+					$(".project1").show()
+					$(".project2").show()
+					$('.project3').addClass('animated slideOutRight');
+					console.log('1')
+				}
+				else if (Object.keys(cleanProjectObject)[0] !== undefined && Object.keys(cleanProjectObject)[1] == undefined){
+					document.getElementById("project1Title").innerHTML = Object.keys(cleanProjectObject)[0];
+					document.getElementById("project1Pic").src = 'images/' + Object.keys(cleanProjectObject)[0] + '.jpg';
+					document.getElementById("project1Caption").innerHTML = cleanProjectCaptions[0];
+						$(".project1").removeClass("animated slideOutRight");
+						$(".project1").show()	
+						$('.project1').addClass('animated slideInRight');
+						$('.project2').addClass('animated slideOutRight');
+						$('.project3').addClass('animated slideOutRight');
+						console.log('2')
+				}
+			// if (Object.keys(cleanProjectObject)[0] && Object.keys(cleanProjectObject)[1] !== undefined && Object.keys(cleanProjectObject)[1] == undefined){
+			// }
+			else{
+				$('.project1').addClass('animated slideOutRight');
 				$('.project2').addClass('animated slideOutRight');
 				$('.project3').addClass('animated slideOutRight');
-				console.log('2')
-		}
-		// if (Object.keys(cleanProjectObject)[0] && Object.keys(cleanProjectObject)[1] !== undefined && Object.keys(cleanProjectObject)[1] == undefined){
-		// }
-		// else{
-		// 	$('.project1').addClass('animated slideOutRight');
-		// 	$('.project2').addClass('animated slideOutRight');
-		// 	console.log('3')
-		// }
+				console.log('3')
+			}
+	}
 }
 
 
